2f35d21e5aa37692e0f8e2326c6f24b5692a79ee
DeepanshuNIT27 <deepanshusahu1615@gmail.com>
2026-05-11 18:44:12 +0530

    integrate socket.io with vite react code editor setup and resolve import issues

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Client from "../components/Client.jsx";
 import Editor from "../components/Editor.jsx";
+import { initSocket } from "../socket.js";
+import { useLocation, useParams } from "react-router-dom";
+import ACTIONS from "../Actions.js";
 
 const EditorPage = () => {
+  const socketRef = useRef(null);
+  const location = useLocation();
+  const { roomId } = useParams();
+
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+    };
+
+    init();
+  }, []);
+
   const [clients, setClients] = useState([
     { socketId: 1, username: "Deepanshu" },
     { socketId: 2, username: "Mayank Raj" },
     { socketId: 3, username: "Mayank Sinha" },
-    
   ]);
 
   return (
