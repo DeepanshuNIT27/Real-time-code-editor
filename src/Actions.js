96281b4fe0a7f08bb277c8690d374f2ee79311ed
DeepanshuNIT27 <deepanshusahu1615@gmail.com>
2026-05-11 18:45:53 +0530

    fix actions export for vite compatibility

--- src/Actions.js
+++ src/Actions.js
@@ -1,11 +1,10 @@
 const ACTIONS = {
-
-    JOIN : 'join',
-    JOINED : 'joined',
-    DISCONNECTED : 'disconnected',
-    CODE_CHANGE : 'code-change',
-    SYNC_CODE :  'sync-code',
-    LEAVE :  'leave',
+  JOIN: "join",
+  JOINED: "joined",
+  DISCONNECTED: "disconnected",
+  CODE_CHANGE: "code-change",
+  SYNC_CODE: "sync-code",
+  LEAVE: "leave",
 };
 
-module.exports = ACTIONS;
+export default ACTIONS;
