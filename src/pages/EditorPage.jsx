514af82cd8f762682e1c24bc65b3e0e681f96120
DeepanshuNIT27 <deepanshusahu1615@gmail.com>
2026-05-12 16:57:29 +0530

    feature: add join notification for users in room

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,33 +1,72 @@
 import React, { useState, useRef, useEffect } from "react";
+import toast from "react-hot-toast";
 import Client from "../components/Client.jsx";
 import Editor from "../components/Editor.jsx";
 import { initSocket } from "../socket.js";
-import { useLocation, useParams } from "react-router-dom";
+
+import {
+  useLocation,
+  useParams,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
+
 import ACTIONS from "../Actions.js";
 
 const EditorPage = () => {
   const socketRef = useRef(null);
+
   const location = useLocation();
+  const reactNavigator = useNavigate();
+
   const { roomId } = useParams();
-
-  useEffect(() => {
-    const init = async () => {
-      socketRef.current = await initSocket();
-
-      socketRef.current.emit(ACTIONS.JOIN, {
-        roomId,
-        username: location.state?.username,
-      });
-    };
-
-    init();
-  }, []);
 
   const [clients, setClients] = useState([
     { socketId: 1, username: "Deepanshu" },
     { socketId: 2, username: "Mayank Raj" },
     { socketId: 3, username: "Mayank Sinha" },
   ]);
+
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+
+      const handleErrors = (e) => {
+        console.log("socket error ", e);
+
+        toast.error("Socket connection failed, try again later.");
+
+        reactNavigator("/");
+      };
+
+      socketRef.current.on("connect_error", handleErrors);
+      socketRef.current.on("connect_failed", handleErrors);
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+
+      // Listening for joined
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+
+          setClients(clients);
+        },
+      );
+    };
+
+    init();
+  }, []);
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <div className="mainWrap">
@@ -47,6 +86,7 @@ const EditorPage = () => {
         </div>
 
         <button className="btn copyBtn">Copy ROOM ID</button>
+
         <button className="btn leaveBtn">Leave</button>
       </div>
 
